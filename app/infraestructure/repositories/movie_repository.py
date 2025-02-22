013d06db49bec0b0c2c1c56e32337191f6bc533b
jfarfa3 <jfarfa3@bancodebogota.com.co>
2025-02-22 09:09:09 -0500

    add movie repository with CRUD operations for movie management

---
+++ app/infraestructure/repositories/movie_repository.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+from app.domain.models import Movie
+
+def get_all_movies(db: Session):
+    return db.query(Movie).all()
+
+def get_movie_by_id(db: Session, movie_id: int):
+    return db.query(Movie).filter(Movie.id == movie_id).first()
+
+def create_movie(db: Session, movie: Movie):
+    db.add(movie)
+    db.commit()
+    db.refresh(movie)
+    return movie
+
+def update_movie(db: Session, movie: Movie):
+    db.query(Movie).filter(Movie.id == movie.id).update(movie)
+    db.commit()
+    db.refresh(movie)
+    return movie
+
+def delete_movie(db: Session, movie: Movie):
+    db.delete(movie)
+    db.commit()
+    return movie
+
