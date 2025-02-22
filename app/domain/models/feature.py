995e8b1942dad49863c19a2fa94f0958dc731ca8
jfarfa3 <jfarfa3@bancodebogota.com.co>
2025-02-22 11:02:53 -0500

    add Feature model to define features in the database

--- app/domain/models/feature.py
+++ app/domain/models/feature.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from app.infrastructure.database import Base
 
-class RoomFeature(Base):
-    __tablename__ = 'room_features'
+class Feature(Base):
+    __tablename__ = 'features'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False, unique=True)
