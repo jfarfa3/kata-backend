e651d7509470b6577263bdadf5ad0d87643b463c
jfarfa3 <jfarfa3@bancodebogota.com.co>
2025-02-22 08:54:19 -0500

    add seat model with attributes and foreign key association to room

---
+++ app/domain/models/seats.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Enum as SqlEnum, ForeignKey
+from app.domain.enums import SeatType
+
+class Seat(Base):
+    __tablename__ = 'seats'
+    id = Column(Integer, primary_key=True, index=True)
+    row = Column(Integer, nullable=False)
+    number = Column(Integer, nullable=False)
+    seat_type = Column(SqlEnum(SeatType), nullable=False, default=SeatType.STANDARD)
+    room_id = Column(Integer, ForeignKey('rooms.id'), nullable=False)
