6fe1aacec3009dee951aabd52e12e37e6a2df510
jfarfa3 <jfarfa3@bancodebogota.com.co>
2025-02-22 08:57:00 -0500

    add showtime model with relationships to movie, room, and seats

---
+++ app/domain/models/showtime.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+class Showtime(Base):
+    __tablename__ = 'showtimes'
+    id = Column(Integer, primary_key=True, index=True)
+    movie_id = Column(Integer, ForeignKey('movies.id'), nullable=False)
+    room_id = Column(Integer, ForeignKey('rooms.id'), nullable=False)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
+    movie = relationship('Movie', backref='showtimes')
+    room = relationship('Room', backref='showtimes')
+    seats = relationship('Seat', backref='showtime')
